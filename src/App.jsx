d4fe4cf90a889faf8c637b766579b5d49b3accb2
Rishabh <rishabhagrawal0623@gmail.com>
2025-08-27 00:54:39 +0530

    chore: add base project structure and initial styles

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route } from "react-router-dom"
+import { Routes } from "react-router"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+
+function App() {
+
+  return (
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* Theme toggle */}
+      {/* Background effect */}
+      {/* Navbar */}
+      {/* Main content */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
