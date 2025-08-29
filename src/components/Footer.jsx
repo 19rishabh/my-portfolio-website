01604cd2c4959142a4651761193426d0e02ca645
Rishabh <rishabhagrawal0623@gmail.com>
2025-08-29 05:49:16 +0530

    chore: minor additions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { ArrowUp } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 items-center">
+    <footer className="py-3 px-4 bg-card relative border-t border-border mt-12 pt-8 items-center">
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
