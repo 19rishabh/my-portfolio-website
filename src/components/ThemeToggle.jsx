a9c4c7c55adee39cb4d551d8ae9f0692e0a6fd7f
Rishabh <rishabhagrawal0623@gmail.com>
2025-08-30 09:30:08 +0530

    refactor: set dark mode as default

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,14 +5,16 @@ import { useEffect } from 'react'
 
 const ThemeToggle = () => {
     const [ isDarkMode, setIsDarkMode  ] = useState(true);
+
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if (storedTheme === "dark") {
+        if (storedTheme === "light") {
+        setIsDarkMode(false);
+        document.documentElement.classList.remove("dark");
+        } else {
         setIsDarkMode(true);
         document.documentElement.classList.add("dark");
-        } else {
-        localStorage.setItem("theme", "light");
-        setIsDarkMode(false);
+        localStorage.setItem("theme", "dark");
         }
     }, []);
   
